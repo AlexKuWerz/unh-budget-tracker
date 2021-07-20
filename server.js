26c1d8a61933ce32727e53e8afcf87ea46b2a102
Oleksandr Kulyk <kylik.aleksandr@gmail.com>
2021-07-19 23:18:39 -0400

    Add server files.

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const logger = require('morgan');
+const path = require('path');
+const compression = require('compression');
+const mongoose = require('./config/connection');
+const routes = require('./controllers');
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.use(logger('dev'));
+app.use(compression());
+app.use(express.urlencoded({
+    extended: true,
+}));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(routes);
+
+mongoose.connection.once('open', () => {
+    app.listen(PORT, () => {
+        console.log(`App running on port ${PORT}!`);
+    });
+});
